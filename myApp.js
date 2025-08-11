42db96444b976b5dd78484cfd82798bb6a5f0aa6
arsh-nav <arshia.nawab@gmail.com>
2025-08-11 18:37:24 +0200

    Eleventh

--- myApp.js
+++ myApp.js
@@ -2,6 +2,7 @@
 let express = require('express');
 let app = express();
 require('dotenv').config();
+let bodyParser = require('body-parser');
 // console.log("Hello world");
 // app.get('/',function(req,res){
 //     res.send("Hello Express")
@@ -9,39 +10,42 @@ require('dotenv').config();
 // let absolutePath = __dirname + '/views/index.html';
 
 
-app.get('/name', function(req, res, next) {
-    res.send({name:req.query.first + " " +req.query.last})
-  next();
-});
+// app.get('/name', function(req, res, next) {
+//     res.send({name:req.query.first + " " +req.query.last})
+//   next();
+// });
 
 
-// let jsonObject = {"message": "Hello json"};
-// console.log(process.env.MESSAGE_STYLE);
+// // let jsonObject = {"message": "Hello json"};
+// // console.log(process.env.MESSAGE_STYLE);
 
-// app.get('/json',function(req, res){
+// // app.get('/json',function(req, res){
     
-//     if(process.env.MESSAGE_STYLE === "uppercase" )
-//         res.json({"message": "Hello json".toUpperCase()});
-//     else
-//         res.json({"message": "Hello json"});
-// })
+// //     if(process.env.MESSAGE_STYLE === "uppercase" )
+// //         res.json({"message": "Hello json".toUpperCase()});
+// //     else
+// //         res.json({"message": "Hello json"});
+// // })
 
-// module.exports = app;
+// // module.exports = app;
 
 
-// 2. Static assets
-app.use('/public', express.static(__dirname + '/public'));
+// // 2. Static assets
+// app.use('/public', express.static(__dirname + '/public'));
 
-// 3. Routes
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+// // 3. Routes
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/views/index.html');
+// });
+
+
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.json());
+app.post('/name', (req, res) => {
+  res.send('Hello ' + req.body.first + ' ' + req.body.last);
 });
-
-
-
-
-
-
 
 
 
